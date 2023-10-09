c56dc017c72e9fbccbfef661ea466c4bca28f9ac
xulihang <xulihanghai@163.com>
2023-10-09 17:30:53 +0800

    update the static files

--- benchmark/assets/QCardActions.7091f2cb.js
+++
@@ -1,1 +0,0 @@
-import{c as t,u as e,k as c,h as n}from"./QBtn.dfccd46a.js";import{c as l,h as i}from"./index.e4c56f4b.js";var d=t({name:"QCardActions",props:{...e,vertical:Boolean},setup(a,{slots:s}){const o=c(a),r=l(()=>`q-card__actions ${o.value} q-card__actions--${a.vertical===!0?"vert column":"horiz row"}`);return()=>i("div",{class:r.value},n(s.default))}});export{d as Q};
---
+++ benchmark/assets/QCardActions.fe094f5a.js
@@ -0,0 +1,1 @@
+import{c as t,u as e,k as c,h as n}from"./QBtn.e78bd08b.js";import{c as l,h as i}from"./index.0b956d2a.js";var d=t({name:"QCardActions",props:{...e,vertical:Boolean},setup(a,{slots:s}){const o=c(a),r=l(()=>`q-card__actions ${o.value} q-card__actions--${a.vertical===!0?"vert column":"horiz row"}`);return()=>i("div",{class:r.value},n(s.default))}});export{d as Q};
