bf69b7310091f6c2112692764e4059fc6ead3bc2
xulihang <xulihanghai@163.com>
2025-01-17 13:52:11 +0800

    use the gtm plugin instead of directly using the js

---
+++ benchmark/assets/QCardActions.adfd1fcc.js
@@ -0,0 +1,1 @@
+import{c as t,u as e,k as c,h as n}from"./QBtn.be621dd2.js";import{c as l,h as i}from"./index.69890ff2.js";var d=t({name:"QCardActions",props:{...e,vertical:Boolean},setup(a,{slots:s}){const o=c(a),r=l(()=>`q-card__actions ${o.value} q-card__actions--${a.vertical===!0?"vert column":"horiz row"}`);return()=>i("div",{class:r.value},n(s.default))}});export{d as Q};
