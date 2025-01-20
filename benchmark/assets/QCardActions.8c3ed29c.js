a5d3d044a03b6401ef620ebaca990770b348a162
xulihang <xulihanghai@163.com>
2025-01-20 13:24:19 +0800

    update static files

---
+++ benchmark/assets/QCardActions.8c3ed29c.js
@@ -0,0 +1,1 @@
+import{c as t,u as e,k as c,h as n}from"./QBtn.8ce15bdf.js";import{c as l,h as i}from"./index.42a38d1c.js";var d=t({name:"QCardActions",props:{...e,vertical:Boolean},setup(a,{slots:s}){const o=c(a),r=l(()=>`q-card__actions ${o.value} q-card__actions--${a.vertical===!0?"vert column":"horiz row"}`);return()=>i("div",{class:r.value},n(s.default))}});export{d as Q};
--- benchmark/assets/QCardActions.cd8f5b04.js
+++
@@ -1,1 +0,0 @@
-import{c as t,u as e,k as c,h as n}from"./QBtn.ff5381ee.js";import{c as l,h as i}from"./index.c6843b6f.js";var d=t({name:"QCardActions",props:{...e,vertical:Boolean},setup(a,{slots:s}){const o=c(a),r=l(()=>`q-card__actions ${o.value} q-card__actions--${a.vertical===!0?"vert column":"horiz row"}`);return()=>i("div",{class:r.value},n(s.default))}});export{d as Q};
